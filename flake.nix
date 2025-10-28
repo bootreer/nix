a63c675896b7639c33549c894001508763935e91
tuomas <typirhonen@icloud.com>
2025-10-28 18:32:50 +0100

    remove firefox-addons input

--- flake.nix
+++ flake.nix
@@ -20,10 +20,6 @@
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
